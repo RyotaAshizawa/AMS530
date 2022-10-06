abc6c79d81997296aa936d75b618479faaaae0af
Ryota Ashizawa <ryota.ashizawa@stonybrook.edu>
2022-10-05 22:10:43 -0400

    myBcast is now implemented

--- HW2/MPI_Bcast/my_bcast_tree.c
+++ HW2/MPI_Bcast/my_bcast_tree.c
@@ -11,6 +11,9 @@ int main(int argc, char **argv) {
     // Variables
     int rank, size, i;
     int tag = 0;
+    int t;
+    int src_rank;
+    int dst_rank;
     double start_time, end_time;
 
     // Get MPI variables
@@ -38,15 +41,13 @@ int main(int argc, char **argv) {
     int max_timestep = ceil(log2(size)) - 1; // # of timesteps required to perform broadcasting. -1 for 0-indexing.
 
     // Loop for each timestep
-    int t;
     for (t = 0; t <= max_timestep; t++) { // t: timestep
         int tot_nodes = pow(2, t); // The total number of nodes that the data should be copied in at this time step
         int rank_start = pow(2, t);  // The smallest process id that should be filled in at this time step
 
         // send data
-        int src_rank;
         for (src_rank = 0; src_rank <= tot_nodes - 1; src_rank++){
-            int dst_rank = src_rank + tot_nodes;
+            dst_rank = src_rank + tot_nodes;
             if (rank == src_rank && dst_rank < size){
                 //printf("Send data from %d to %d\n", src_rank, dst_rank);
                 MPI_Isend(&arr, N, MPI_FLOAT, dst_rank, tag, MPI_COMM_WORLD, &request);
@@ -54,9 +55,8 @@ int main(int argc, char **argv) {
         }
 
         // receive data
-        int dst_rank;
         for (dst_rank = rank_start; dst_rank <= (rank_start + tot_nodes - 1); dst_rank++){
-            int src_rank = dst_rank - tot_nodes;
+            src_rank = dst_rank - tot_nodes;
             if (rank == dst_rank && dst_rank < size){
                 //printf("Recv from %d to %d\n", src_rank, dst_rank);
                 MPI_Irecv(&arr, N, MPI_FLOAT, src_rank, tag, MPI_COMM_WORLD, &request);
