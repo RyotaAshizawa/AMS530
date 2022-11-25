236e5c085ed8e1389e505207ca6a961cd93ee4bb
Ryota Ashizawa <ryota.ashizawa@stonybrook.edu>
2022-11-25 01:00:14 -0500

    jj

--- HW4_in_c/main.c
+++ HW4_in_c/main.c
@@ -124,7 +124,6 @@ int main(int argc, char **argv) {
     if (rank == 0) {
         mpi_send_n_particles_to_eachrank(map_rank_to_n_particles_in_surrcells, tag, max_rank, MPI_COMM_WORLD, &request);
     }
-    /**
     if (rank < max_rank) {
         MPI_Irecv(map_rank_to_n_particles_in_surrcells, max_rank, MPI_INT, 0, tag, MPI_COMM_WORLD, &request);
         MPI_Wait(&request, &status);
@@ -144,6 +143,7 @@ int main(int argc, char **argv) {
         }
     }
 
+    /**
 
 
     //// 7. Calculate the force of each main box
