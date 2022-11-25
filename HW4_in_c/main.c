6086a4bf2c795cb92a7c5dffc6356e90614fd7cd
Ryota Ashizawa <ryota.ashizawa@stonybrook.edu>
2022-11-25 01:05:27 -0500

    jj

--- HW4_in_c/main.c
+++ HW4_in_c/main.c
@@ -133,10 +133,8 @@ int main(int argc, char **argv) {
     if (rank == 0) {
         map_rank_to_coords_surrcells(max_rank, map_rank_to_n_particles_in_surrcells, map_rank_to_coords_surrbox, coords_each_rank, n_particles_eachrank, map_rank_to_n_surrcells, map_rank_to_ranks_of_surrcells);
         //mpi_send_surrbox_particles(map_rank_to_coords_surrbox, map_rank_to_n_particles_in_surrcells, max_rank, MPI_COMM_WORLD, &request, tag);
-    }
-    if (rank == 16) {
-        printf("%d particles are received for the peripheral box for rank %d:\n", map_rank_to_n_particles_in_surrcells[rank], rank);
-        print_particles_in_box(map_rank_to_coords_surrbox[rank], map_rank_to_n_particles_in_surrcells[rank]);
+        printf("%d particles are received for the peripheral box for rank %d:\n", map_rank_to_n_particles_in_surrcells[16], 16);
+        print_particles_in_box(map_rank_to_coords_surrbox[16], map_rank_to_n_particles_in_surrcells[16]);
     }
     /**
     if (rank < max_rank) {
