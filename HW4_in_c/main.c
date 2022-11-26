4f0b904b758226f692ec5d859c47152b294c6f2b
Ryota Ashizawa <ryota.ashizawa@stonybrook.edu>
2022-11-26 16:35:22 -0500

    Memory release functionality is now implemented

--- HW4_in_c/main.c
+++ HW4_in_c/main.c
@@ -133,9 +133,10 @@ int main(int argc, char **argv) {
     //// 6. Send-recv cooridinates of surr cells
     if (rank == 0) {
         map_rank_to_coords_surrcells(max_rank, map_rank_to_n_particles_in_surrcells, map_rank_to_coords_surrbox, coords_each_rank, n_particles_eachrank, map_rank_to_n_surrcells, map_rank_to_ranks_of_surrcells, false, rank_interest);
-        mpi_send_surrbox_particles(map_rank_to_coords_surrbox, map_rank_to_n_particles_in_surrcells, max_rank, MPI_COMM_WORLD, &request, tag);
+        //mpi_send_surrbox_particles(map_rank_to_coords_surrbox, map_rank_to_n_particles_in_surrcells, max_rank, MPI_COMM_WORLD, &request, tag);
         print_particles_in_box(map_rank_to_coords_surrbox[rank_interest], map_rank_to_n_particles_in_surrcells[rank_interest]);
     }
+    /**
     if (rank < max_rank) {
         MPI_Irecv(coords_peripheral_box, map_rank_to_n_particles_in_surrcells[rank] * 4, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD, &request);
         MPI_Wait(&request, &status);
@@ -146,7 +147,6 @@ int main(int argc, char **argv) {
     }
 
 
-    /**
     //// 7. force debugger
     if (rank == 0) {
         double *coords1 = (double *) malloc(sizeof(double) * 4);
