724086820d97e7a16b830f413cfdc162b327a18d
Ryota Ashizawa <ryota.ashizawa@stonybrook.edu>
2022-11-26 13:27:39 -0500

    Memory release functionality is now implemented

--- HW4_in_c/main.c
+++ HW4_in_c/main.c
@@ -83,17 +83,17 @@ int main(int argc, char **argv) {
         assign_rank_to_cell(box, N, n_particles_eachrank, map_cell_to_rank, cpus_per_side, cell_len_per_cpu, max_rank);
         print_particles(box, N); // check it
     }
-    /**
 
     //// 2. Send-Recv number of particles of each cell
     if (rank == 0) {
         mpi_send_n_particles_to_eachrank(n_particles_eachrank, tag, max_rank, MPI_COMM_WORLD, &request);
     }
-    if (rank < max_rank) {
+    elif (rank < max_rank) {
         MPI_Irecv(n_particles_eachrank, max_rank, MPI_INT, 0, tag, MPI_COMM_WORLD, &request);
         MPI_Wait(&request, &status);
         //printf("Rank:%d, Recv:%d\n", rank, n_particles_eachrank[0]);
     }
+    /**
 
     //// 3. Send-recv particle positions of each cell
     if (rank == 0) {
