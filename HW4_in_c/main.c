3dea7cfe79754abec9394dcb37f25f4ab0595cf5
Ryota Ashizawa <ryota.ashizawa@stonybrook.edu>
2022-11-26 15:09:13 -0500

    Memory release functionality is now implemented

--- HW4_in_c/main.c
+++ HW4_in_c/main.c
@@ -124,6 +124,7 @@ int main(int argc, char **argv) {
         MPI_Wait(&request, &status);
     }
     printf("Rank:%d, N of particles in the surrownding cells:%d\n", rank, map_rank_to_n_particles_in_surrcells[rank]);
+    /**
 
     //// 6. Send-recv cooridinates of surr cells
     // allocate memory first
@@ -145,7 +146,6 @@ int main(int argc, char **argv) {
         }
     }
 
-    /**
 
     //// 7. force debugger
     if (rank == 0) {
