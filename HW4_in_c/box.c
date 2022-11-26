2ed2a03c5e74cfa6a6258467751a1becb08d6ad6
Ryota Ashizawa <ryota.ashizawa@stonybrook.edu>
2022-11-26 15:54:49 -0500

    Memory release functionality is now implemented

--- HW4_in_c/box.c
+++ HW4_in_c/box.c
@@ -258,12 +258,14 @@ void map_rank_to_coords_surrcells(int max_rank, int *map_rank_to_n_particles_in_
     }
     if (print_option) {
         printf("Particle coords in the surrownding cell for rank %d.\n", print_rank);
-        printf("%d, (x, y, z, id) = (%.2f, %.2f, %.2f, %d)\n",
-               processed_temp_n_particles,
-               map_rank_to_coords_surrbox[print_rank][processed_temp_n_particles * 4 + 0],
-               map_rank_to_coords_surrbox[print_rank][processed_temp_n_particles * 4 + 1],
-               map_rank_to_coords_surrbox[print_rank][processed_temp_n_particles * 4 + 2],
-               (int)map_rank_to_coords_surrbox[print_rank][processed_temp_n_particles * 4 + 3]);
+        for (int i = 0; i < map_rank_to_n_particles_in_surrcells[i]; i++) {
+            printf("%d, (x, y, z, id) = (%.2f, %.2f, %.2f, %d)\n",
+                   processed_temp_n_particles,
+                   map_rank_to_coords_surrbox[print_rank][i * 4 + 0],
+                   map_rank_to_coords_surrbox[print_rank][i * 4 + 1],
+                   map_rank_to_coords_surrbox[print_rank][i * 4 + 2],
+                   (int) map_rank_to_coords_surrbox[print_rank][i * 4 + 3]);
+        }
     }
 
 }
